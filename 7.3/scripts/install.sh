90a0434f492d1057ace31c0ec8b0d7b1f7a5e824
BlackEagle <ike.devolder@gmail.com>
2018-12-07 17:21:31 +0100

    add php 7.3
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

---
+++ 7.3/scripts/install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+# install packages
+apt-get update
+
+extensions -i bcmath gd intl mbstring mysql readline soap xsl
+
+apt-get clean -y
+
+# magento needs a bit more memory than 256M
+sed -e 's/^\(memory_limit\).*/\1 = 1024M/' \
+    -i /etc/php/${DW_PHP_VERSION}/mods-available/custom.ini
+
+sed -e 's/\(max_nesting_level\).*/\1=1000/' \
+    -i /etc/php/${DW_PHP_VERSION}/mods-available/xdebug.ini
+
+# install magerun
+curl -sS -o /usr/local/bin/magerun https://files.magerun.net/n98-magerun2.phar
+chmod +x /usr/local/bin/magerun
+
+# update permissions to allow rootless operation
+/usr/local/bin/permissions
